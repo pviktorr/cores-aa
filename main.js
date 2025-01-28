f46a523494b746dfc1378d608f71fe056c63e27e
PEDRO VICTOR RODRIGUES DE ALMEIDA <24122478@CFP127ALUNO.LOCAL>
2025-01-28 15:58:36 -0300

    javaScript

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+function trocaCor () {
+    const cor = document.getElementById('cor').value 
+    document.documentElement.style.setProperty('--cor-fundo', cor)
+}
+
+botaoTrocarCor.addEventListener('click', trocaCor)
+
